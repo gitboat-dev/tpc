3053391ea778b25998e76e329df107355afd7d71
Boat <gitboat.dev@gmail.com>
2022-03-14 15:28:07 +0700

    14-03-2022 15:27 By Boat

--- resources/views/products/ring-spun.blade.php
+++ resources/views/products/ring-spun.blade.php
@@ -11,19 +11,140 @@
 					<div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
                         <div class="img_block1">
 							<picture>
-								<source srcset="\assets\images\ring-spun\mobile\Head-Ring-Spun-Thaipolyester.webp" alt="Polyester Staple Fiber (PSF) Thaipolyester" media="(max-width: 640px)">
-								<img src="\assets\images\ring-spun\desktop\Head-Ring-Spun-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Polyester Staple Fiber (PSF) Thaipolyester">
+								<source srcset="\assets\images\ring-spun\mobile\Head-Ring-Spun-Thaipolyester.webp" alt="Ring Spun Thaipolyester" media="(max-width: 640px)">
+								<img src="\assets\images\ring-spun\desktop\Head-Ring-Spun-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Ring Spun Thaipolyester">
 					        </picture>
 					    </div>
                     </div>
                 </div>
             </div>
-            <div class="section-two">
-
+            <div class="section-two bg-black">
+                <div class="row m-0 r1">
+                    <div class="col-xs-12 col-sm-12 col-md-8 p-0 col1">
+                        111
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4 p-0 col1">
+                        222
+                    </div>
+                </div>
+            </div>
+            <div class="section-three bg-black">
+                <div class="row m-0 r1">
+                    <div class="col-xs-12 col-sm-12 col-md-8 p-0 col1">
+                        111
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4 p-0 col1">
+                        222
+                    </div>
+                </div>
+            </div>
+            <div class="section-four bg-black">
+                <div class="row m-0 r1">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                        <div class="content_1">
+                            <h2 class="text-green">The spindle serves three functions:</h2>
+                            <p class="p1">
+                                First, it provides a location to wind a package;
+                            </p>
+                            <p class="p2">
+                                Secondly, by rotating the yarn package, the spindle causes twist to be inserted into the strand of yarn being formed at the nip of the front rolls;
+                            </p>
+                            <p class="p3">
+                                Thirdly, the rotation of the package causes the yarn to pull the traveler around the ring, providing a method of not only transmitting twist derived from the spindle, but also a guide to
+                                change the direction of yarn travel so that it approaches the yarn package tangentially, and thus can be wound onto the package.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-five bg-black">
+                <div class="row m-0 r1">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                        <div class="content_1">
+                            <h2 class="text-white">Limitations: Mechanically speaking, the production of a ring
+                                frame is limited by excesses in one or more of the following:</h2>
+                            <ul class="data-lists">
+                                <li>a) Spindle speed</li>
+                                <li>b) Front roll speed</li>
+                                <li>c) Traveler speed</li>
+                                <li>d) Frame down-time for doffing</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-six">
+                <div class="row m-0 r1">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                        <div class="content_1">
+                            <p class="p1">
+                                Economic limitations on ring spinning are related to power consumption and package size. Power requirements to rotate the package are more significant than insert twists only.
+                                In addition, the package size is limited since it must be enclosed in the yarn balloon. Finally, for very coarse yarn counts, frame down-time for doffing must be considered since it
+                                increases as coarser yarns are spun.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-seven bg-black">
+                <div class="row m-0 r1">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                        <div class="content_1">
+                            <p class="p1">
+                                Spun yarn luster<br>
+                                Luster refers to the degree of light reflected from<br>
+                                the surface of a fiber or the degree of gloss or<br>
+                                sheen that the fiber possesses.<br>
+                                - Full dull<br>
+                                - Semi-dull
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-eight">
+                <div class="row m-0 r1">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                        <ul class="image-lists row">
+                            <li class="col-xs-3 col-sm-3 col-md-3 p-0">
+                                <div class="img_block1">
+                                    <picture>
+                                        <source srcset="\assets\images\ring-spun\mobile\Ring-Spun-Product-dopedyed-black-Thaipolyester.webp" alt="Ring Spun Product dopedyed black Thaipolyester" media="(max-width: 640px)">
+                                        <img src="\assets\images\ring-spun\desktop\Ring-Spun-Product-dopedyed-black-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Ring Spun Product dopedyed black Thaipolyester">
+                                    </picture>
+                                </div>
+                            </li>
+                            <li class="col-xs-3 col-sm-3 col-md-3 p-0">
+                                <div class="img_block1">
+                                    <picture>
+                                        <source srcset="\assets\images\ring-spun\mobile\Ring-Spun-Product-topdyed-Thaipolyester.webp" alt="Ring Spun Product topdyed Thaipolyester" media="(max-width: 640px)">
+                                        <img src="\assets\images\ring-spun\desktop\Ring-Spun-Product-topdyed-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Ring Spun Product topdyed Thaipolyester">
+                                    </picture>
+                                </div>
+                            </li>
+                            <li class="col-xs-3 col-sm-3 col-md-3 p-0">
+                                <div class="img_block1">
+                                    <picture>
+                                        <source srcset="\assets\images\ring-spun\mobile\Ring-Spun-Product-topdyed-Thaipolyester-1.webp" alt="Ring Spun Product dopedyed black Thaipolyester" media="(max-width: 640px)">
+                                        <img src="\assets\images\ring-spun\desktop\Ring-Spun-Product-topdyed-Thaipolyester-1.webp" width="900" height="350" class="img-fluid ab-img" alt="Ring Spun Product dopedyed black Thaipolyester">
+                                    </picture>
+                                </div>
+                            </li>
+                            <li class="col-xs-3 col-sm-3 col-md-3 p-0">
+                                <div class="img_block1">
+                                    <picture>
+                                        <source srcset="\assets\images\ring-spun\mobile\Ring-Spun-Product-Raw-White-Thaipolyester.webp" alt="Ring Spun Product Raw White Thaipolyester" media="(max-width: 640px)">
+                                        <img src="\assets\images\ring-spun\desktop\Ring-Spun-Product-Raw-White-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Ring Spun Product Raw White Thaipolyester">
+                                    </picture>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 @endsection
 @section('custom-script')
-	<script src="/js/ring-spun.js?v={{ time() }}"></script>
+	{{-- <script src="/js/ring-spun.js?v={{ time() }}"></script> --}}
 @endsection
