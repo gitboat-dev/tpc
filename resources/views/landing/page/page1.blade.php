a6701261838e7885adca44dece90f799e3c8f5ba
Boat <gitboat.dev@gmail.com>
2022-03-15 17:02:06 +0700

    15-03-2022 17:01 By Boat

--- resources/views/landing/page/page1.blade.php
+++ resources/views/landing/page/page1.blade.php
@@ -5,16 +5,189 @@
 @endsection
 @section('content')
 <div id="landing">
-    <section class="sec1">
+    <section id="sec1">
         <div class="row r1 m-0">
-            <div class="class=col-xs-12 col-sm-12 col-md-12 p-0">
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0">
                 <div class="img_block">
                     <picture>
-                        <source srcset="\assets\images\ring-spun\mobile\Head-Ring-Spun-Thaipolyester.webp" alt="Ring Spun Thaipolyester" media="(max-width: 640px)">
-                        <img src="\assets\images\ring-spun\desktop\Head-Ring-Spun-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Ring Spun Thaipolyester">
+                        <source srcset="\assets\images\landing\page1\mobile\Head-Landingpage-Thaipolyester.webp" alt="Landingpage Thaipolyester" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\Head-Landingpage-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Landingpage Thaipolyester">
                     </picture>
                 </div>
             </div>
+        </div>
+    </section>
+    <section id="sec2" class="bg-black">
+        <div class="row r1 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group m-auto">
+                        <p class="p1">
+                            To achieve that, you need to have an expert system in the production process that
+                            produces superior products! Because of the high technology used in our production line,
+                            <label class="green-hl">your defective rate will be reduced.</label> Our ready-to-ship stock can <label class="green-hl">save you a lot of time.</label>
+                            And typically, the superiority of having a GRS certificate to guarantee that your product
+                            is genuinely recycled can <label class="green-hl">add more value to your product and is trusted globally.</label>
+                        </p>
+                        <p class="p2 text-center">
+                            These explain why we are specialized in the textile industry,<br>
+                            with expertise in POLYESTER.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="sec3">
+        <div class="row r1 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-5-5 p-0 col1">
+                <div class="row r1-1 m-0">
+                    <div class="col-md-12 p-0 col1-1">
+                        <div class="img_block">
+                            <picture>
+                                <source srcset="\assets\images\landing\page1\mobile\Ring-Spun-Process-Lindingpage-Thaipolyester.webp" alt="Ring Spun Process Thaipolyester" media="(max-width: 640px)">
+                                <img src="\assets\images\landing\page1\desktop\Ring-Spun-Process-Lindingpage-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Ring Spun Process Thaipolyester">
+                            </picture>
+                        </div>
+                    </div>
+                    <div class="col-md-12 p-0 col1-2">
+                        <div class="img_block">
+                            <picture>
+                                <source srcset="\assets\images\landing\page1\mobile\landing-page-Product-Draw-textured-yarn-Thai-Polyester-TPC-1.webp" alt="Product Draw textured yarn Thai Polyester TPC" media="(max-width: 640px)">
+                                <img src="\assets\images\landing\page1\desktop\landing-page-Product-Draw-textured-yarn-Thai-Polyester-TPC-1.webp" width="900" height="350" class="img-fluid ab-img" alt="Product Draw textured yarn Thai Polyester TPC">
+                            </picture>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6-5 p-0 col2">
+                <div class="img_block">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\mobile\landing-page-Product-Draw-textured-yarn-Thai-Polyester-TPC.webp" alt="Product Draw textured yarn Thai Polyester TPC" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\landing-page-Product-Draw-textured-yarn-Thai-Polyester-TPC.webp" width="900" height="350" class="img-fluid ab-img" alt="Product Draw textured yarn Thai Polyester TPC">
+                    </picture>
+                </div>
+            </div>
+    </section>
+    <section id="sec4">
+        <div class="row r1 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                <h2 class="text-center">
+                    <label class="theme-hl">100% POLYESTER YARN</label><br>
+                    PRODUCT LIST
+                </h2>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                <div class="content_block pos-r">
+                    <div class="img_block">
+                        <picture>
+                            <source srcset="\assets\images\landing\page1\mobile\Filament-Yarn-landing-page-TPC-Thaipolyester.webp" alt="Filament Yarn TPC Thaipolyester" media="(max-width: 640px)">
+                            <img src="\assets\images\landing\page1\desktop\Filament-Yarn-landing-page-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Filament Yarn TPC Thaipolyester">
+                        </picture>
+                    </div>
+                    <div class="img_block">
+                        <picture>
+                            <source srcset="\assets\images\landing\page1\mobile\Spun-Yarn-landing-page-TPC-Thaipolyester.webp" alt="Spun Yarn TPC Thaipolyester" media="(max-width: 640px)">
+                            <img src="\assets\images\landing\page1\desktop\Spun-Yarn-landing-page-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Spun Yarn TPC Thaipolyester">
+                        </picture>
+                    </div>
+                    <div class="img_block">
+                        <picture>
+                            <source srcset="\assets\images\landing\page1\mobile\Polyester-Staple-Fiber-landing-page-TPC-Thaipolyester.webp" alt="Polyester Staple Fiber TPC Thaipolyester" media="(max-width: 640px)">
+                            <img src="\assets\images\landing\page1\desktop\Polyester-Staple-Fiber-landing-page-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Polyester Staple Fiber TPC Thaipolyester">
+                        </picture>
+                    </div>
+                </div>
+            </div>
+    </section>
+    <section id="sec5">
+        <ul class="row m-0">
+            <li class="col-xs-3 col-sm-3 col-md-1-5 p-0">
+                <div class="img_block">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\mobile\Polyester-Partially-Oriented-Yarn-landing-page-TPC-Thaipolyester.webp" alt="Polyester Partially Oriented Yarn TPC Thaipolyester" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\Polyester-Partially-Oriented-Yarn-landing-page-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Polyester Partially Oriented Yarn TPC Thaipolyester">
+                    </picture>
+                </div>
+            </li>
+            <li class="col-xs-3 col-sm-3 col-md-1-5 p-0">
+                <div class="img_block">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\mobile\Draw-textured-Yarn-landing-page-TPC-Thaipolyester.webp" alt="Draw textured Yarn TPC Thaipolyester" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\Draw-textured-Yarn-landing-page-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Draw textured Yarn TPC Thaipolyester">
+                    </picture>
+                </div>
+            </li>
+            <li class="col-xs-3 col-sm-3 col-md-1-5 p-0">
+                <div class="img_block">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\desktop\Fully-Drawn-Yarn-Dope-Dyed-Black-landing-page-TPC-Thaipolyester.webp" alt="Fully Drawn Yarn Dope Dyed Black TPC Thaipolyester" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\Fully-Drawn-Yarn-Dope-Dyed-Black-landing-page-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Fully Drawn Yarn Dope Dyed Black TPC Thaipolyester">
+                    </picture>
+                </div>
+            </li>
+            <li class="col-xs-3 col-sm-3 col-md-1-5 p-0">
+                <div class="img_block">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\mobile\Twisting-Yarn-landing-page-Raw-white-TPC-Thaipolyester.webp" alt="Twisting Yarn Raw white TPC Thaipolyester" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\Twisting-Yarn-landing-page-Raw-white-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Twisting Yarn Raw white TPC Thaipolyester">
+                    </picture>
+                </div>
+            </li>
+            <li class="col-xs-3 col-sm-3 col-md-1-5 p-0">
+                <div class="img_block">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\mobile\Twisting-Yarn-landing-page-Dope-Dyed-Black-TPC-Thaipolyester.webp" alt="Twisting Yarn Dope Dyed Black TPC Thaipolyester" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\Twisting-Yarn-landing-page-Dope-Dyed-Black-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Twisting Yarn Dope Dyed Black TPC Thaipolyester">
+                    </picture>
+                </div>
+            </li>
+            <li class="col-xs-3 col-sm-3 col-md-1-5 p-0">
+                <div class="img_block">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\mobile\Ring-Spun-Yarn-landing-page-Raw-white-TPC-Thaipolyester.webp" alt="Ring Spun Yarn Raw white TPC Thaipolyester" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\Ring-Spun-Yarn-landing-page-Raw-white-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Ring Spun Yarn Raw white TPC Thaipolyester">
+                    </picture>
+                </div>
+            </li>
+            <li class="col-xs-3 col-sm-3 col-md-1-5 p-0">
+                <div class="img_block">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\mobile\Open-End-Spun-Yarn-landing-page-Dope-Dyed-Black-TPC-Thaipolyester.webp" alt="Open End Spun Yarn Dope Dyed Black TPC Thaipolyester" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\Open-End-Spun-Yarn-landing-page-Dope-Dyed-Black-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Open End Spun Yarn Dope Dyed Black TPC Thaipolyester">
+                    </picture>
+                </div>
+            </li>
+            <li class="col-xs-3 col-sm-3 col-md-1-5 p-0">
+                <div class="img_block">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\mobile\Polyester-Staple-Fiber-landing-page-Raw-white-TPC-Thaipolyester.webp" alt="Polyester Staple Fiber Raw white TPC Thaipolyester" media="(max-width: 640px)">
+                        <img src="\assets\images\landing\page1\desktop\Polyester-Staple-Fiber-landing-page-Raw-white-TPC-Thaipolyester.webp" width="900" height="350" class="img-fluid ab-img" alt="Polyester Staple Fiber Raw white TPC Thaipolyester">
+                    </picture>
+                </div>
+            </li>
+        </ul>
+    </section>
+    <section id="sec6">
+        <div class="row r1 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                <div class="content_block d-flex pos-r">
+                    <h2 class="theme-hl text-center m-auto">
+                        Why not go with the right supplier?<br>
+                        Who is always ready to provide you with<br>
+                        superior products and services!
+                    </h2>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0 col2">
+                <div class="content_block d-flex pos-r">
+                    <p class="p1 text-center m-auto">
+                        You will have no risks using our products; the only risk you’d have is not choosing us.<br>
+                        So don’t let your competitor get a better opportunity.
+                    </p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0"></div>
         </div>
     </section>
 </div>
