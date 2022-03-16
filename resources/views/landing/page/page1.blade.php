ba3249a23ab1bb357f52d3244c926a399e964b0e
Boat <gitboat.dev@gmail.com>
2022-03-16 17:01:29 +0700

    16-03-2022 17:01 By Boat

--- resources/views/landing/page/page1.blade.php
+++ resources/views/landing/page/page1.blade.php
@@ -1,7 +1,7 @@
 @extends('landing.template')
 @section('style')
     <link rel="preload stylesheet" href="\css\landing\page\page1.css" as="style" crossorigin>
-
+    <link rel="stylesheet" id="fancybox-css" href="{!!asset('/assets/custom/wp-content/plugins/radiantthemes-addons/assets/css/jquery.fancybox.min.css?ver=4.9.11')!!}" type="text/css" media="all">
 @endsection
 @section('content')
 <div id="landing">
@@ -401,9 +401,215 @@
         </div>
     </section>
     <section id="sec15" class="bg-black">
-
+        <div class="row r1 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group m-auto">
+                        <h2 class="theme-hl text-center">
+                            Get FREE SAMPLE<br>
+                            Now!
+                        </h2>
+                        <form id="contact-form" data-request="landing-page" autocomplete="off">
+                            <div class="form-group col-xs-12 col-sm-12 col-md-12">
+                                <input type="text" class="form-control" name="name" id="name" placeholder="NAME*">
+                                <span class="text-danger name mb-font12"></span>
+                            </div>
+                            <div class="form-group col-xs-12 col-sm-12 col-md-12">
+                                <input type="text" class="form-control" name="company" id="company" placeholder="COMPANY">
+                                <span class="text-danger company mb-font12"></span>
+                            </div>
+                            <div class="form-group col-xs-6 col-sm-6 col-md-6">
+                            <input type="email" class="form-control" name="email" id="email" placeholder="EMAIL*">
+                                <span class="text-danger email mb-font12"></span>
+                            </div>
+                            <div class="form-group col-xs-6 col-sm-6 col-md-6">
+                                <input type="text" class="form-control" name="phone" id="phone" placeholder="PHONE">
+                                <span class="text-danger phone mb-font12"></span>
+                            </div>
+                            <div class="form-group col-xs-12 col-sm-12 col-md-12">
+                                <textarea class="form-control" name="message" id="message" placeholder="MESSAGE" rows="5"></textarea>
+                                <span class="text-danger message mb-font12"></span>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div id="res" class="res"></div>
+                            </div>
+                            <div class="form-group col-xs-12 col-sm-12 col-md-12 text-center">
+                                <button type="button" id="submitcontract2" class="bt-contact">SEND</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="sec16" class="bg-gray">
+        <div class="row r1 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                <div class="content_block d-flex pos-r">
+                    <h2 class="text-center">
+                        <i>Get in touch with us !</i>
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row r2 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-3 p-0 col1">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group">
+                        <div class="img_block">
+                            <picture>
+                                <source srcset="\assets\images\landing\page1\mobile\Icon-landing-page-TPC-Thai-Polyester-Phone.webp" alt="Phone" media="(max-width: 640px)">
+                                <img src="\assets\images\landing\page1\desktop\Icon-landing-page-TPC-Thai-Polyester-Phone.webp" width="900" height="350" class="img-fluid ab-img" alt="Phone">
+                            </picture>
+                        </div>
+                        <h3 class="theme-hl text-center">Phone :</h3>
+                        <p class="text-center">+66 34 852371 to 2</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 p-0 col2">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group">
+                        <div class="img_block">
+                            <picture>
+                                <source srcset="\assets\images\landing\page1\mobile\Icon-landing-page-TPC-Thai-Polyester-Email.webp" alt="Email" media="(max-width: 640px)">
+                                <img src="\assets\images\landing\page1\desktop\Icon-landing-page-TPC-Thai-Polyester-Email.webp" width="900" height="350" class="img-fluid ab-img" alt="Email">
+                            </picture>
+                        </div>
+                        <h3 class="theme-hl text-center">Email :</h3>
+                        <p class="text-center">support@thaipolyester.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 p-0 col3">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group">
+                        <div class="img_block">
+                            <picture>
+                                <source srcset="\assets\images\landing\page1\mobile\Icon-landing-page-TPC-Thai-Polyester-Website.webp" alt="Website" media="(max-width: 640px)">
+                                <img src="\assets\images\landing\page1\desktop\Icon-landing-page-TPC-Thai-Polyester-Website.webp" width="900" height="350" class="img-fluid ab-img" alt="Website">
+                            </picture>
+                        </div>
+                        <h3 class="theme-hl text-center">Website :</h3>
+                        <p class="text-center">www.thaipolyester.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 p-0 col4">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group">
+                        <div class="img_block">
+                            <picture>
+                                <source srcset="\assets\images\landing\page1\mobile\Icon-landing-page-TPC-Thai-Polyester-Whatsapp.webp" alt="Whatsapp" media="(max-width: 640px)">
+                                <img src="\assets\images\landing\page1\desktop\Icon-landing-page-TPC-Thai-Polyester-Whatsapp.webp" width="900" height="350" class="img-fluid ab-img" alt="Whatsapp">
+                            </picture>
+                        </div>
+                        <h3 class="theme-hl text-center">Whatsapp :</h3>
+                        <p class="text-center">+66 63 902 1369</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row r3 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-3 p-0 col1">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group">
+                        <div class="img_block">
+                            <picture>
+                                <source srcset="\assets\images\landing\page1\mobile\Icon-landing-page-TPC-Thai-Polyester-Address.webp" alt="Address" media="(max-width: 640px)">
+                                <img src="\assets\images\landing\page1\desktop\Icon-landing-page-TPC-Thai-Polyester-Address.webp" width="900" height="350" class="img-fluid ab-img" alt="Address">
+                            </picture>
+                        </div>
+                        <h3 class="theme-hl text-center">Address :</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 p-0 col2">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group">
+                        <h3 class="theme-hl text-center">Head Office :</h3>
+                        <p class="text-center">
+                            470 Bangkuntien-Chaitaley<br>
+                            Rd., Samaedum, Bangkuntien,<br>
+                            Bangkok 10150 Thailand
+                        </p>
+                        <div class="text-center">
+                            <a class="a-map" title="Map Head Office Thai Polyester" href="/assets/images/contact/TPC Map-02.webp" data-fancybox="gallery" data-thumbnail-src="/assets/images/contact/TPC Map-02.webp">
+                                Map
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 p-0 col3">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group">
+                        <h3 class="theme-hl text-center">Factory :</h3>
+                        <p class="text-center">
+                            888 Moo1, Highway 331 Km.65<br>
+                            (Ban Nong Phai Kaeo), Ban Bung,<br>
+                            Chonburi 20220 Thailand
+                        </p>
+                        <div class="text-center">
+                            <a class="a-map" title="Map Factory Thai Polyester" href="/assets/images/contact/TPC Map-03.webp" data-fancybox="gallery" data-thumbnail-src="/assets/images/contact/TPC Map-03.webp">
+                                Map
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 p-0 col4">
+                <div class="content_block d-flex pos-r">
+                    <div class="content_group">
+                        <h3 class="theme-hl text-center">Sales Office :</h3>
+                        <p class="text-center">
+                            Jongstit Park 99 Moo1,<br>
+                            Soi Wat Phojae, Ekachai Rd.,<br>
+                            Khaerai, Kratumban,<br>
+                            Samutsakorn 74110 Thailand
+                        </p>
+                        <div class="text-center">
+                            <a class="a-map" title="Map Sale Office Office Thai Polyester" href="/assets/images/contact/TPC Map-01.webp" data-fancybox="gallery" data-thumbnail-src="/assets/images/contact/TPC Map-01.webp">
+                                Map
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row r4 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                <div class="content_block d-flex pos-r">
+                    <p class="p1 text-center m-auto">
+                        copyr ight c 2019 Thai polyes ter Co. ,Ltd. Al l r ight s reserved.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="sec17" class="bg-gray">
+        <div class="row r1 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                <div class="content_block d-flex pos-r">
+                    <h2 class="text-center">
+                        About US
+                    </h2>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 p-0 col2">
+                <div class="content_block d-flex pos-r">
+                    <p class="p1 m-auto">
+                        Thai Polyester Company (TPC) was founded in 2001. We are a vertically integrated fiber polyester
+                        manufacturer with a total capacity of 316,800 tons per year, utilizing innovative technology
+                        worldwide. TPC is a registered company specializing in polyester yarn, fiber, and filament. We
+                        export polyester yarn to a variety of countries. We use high-tech German machine in our process.
+                    </p>
+                </div>
+            </div>
+        </div>
     </section>
 </div>
 @endsection
 @section('script')
+    <script src="/assets/custom/wp-content/plugins/radiantthemes-addons/assets/js/jquery.fancybox.min.js?v={{ time() }}"></script>
 @endsection
