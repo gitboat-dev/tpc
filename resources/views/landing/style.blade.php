e8d7188a35e2de38280916ba53ac6e2557a39a01
Boat <gitboat.dev@gmail.com>
2022-03-21 15:25:10 +0700

    21-03-2022 15:25 By Boat

--- resources/views/landing/style.blade.php
+++ resources/views/landing/style.blade.php
@@ -3,6 +3,6 @@
 <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/assets/lib/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<link rel="stylesheet"  href="/css/landing/template.css" type="text/css" media="all">
-<link rel="stylesheet"  href="/css/landing/scrollup.css" type="text/css" media="all">
+<link rel="stylesheet"  href="/css/landing/template.min.css" type="text/css" media="all">
+<link rel="stylesheet"  href="/css/landing/scrollup.min.css" type="text/css" media="all">
 <link rel="stylesheet" id="contact-tools-css" href="/css/landing/contact-tools.min.css" type="text/css" media="all">
