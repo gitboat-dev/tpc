0d58e12e806519914ebc54e0d13b397e32d05ac2
Boat <gitboat.dev@gmail.com>
2022-03-22 10:25:02 +0700

    22-03-2022 10:24 By Boat

--- routes/landing.php
+++ routes/landing.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'landing'], function () {
     Route::get('/High-Quality-Polyester-Yarn-and-Fiber', 'landingPageController@page1');
-    Route::get('/High-Quality-Polyester-Yarn-and-Fiber/thank', 'landingPageController@thank_you1');
+    Route::get('/landing/thank', 'landingPageController@thank_you1');
 });
