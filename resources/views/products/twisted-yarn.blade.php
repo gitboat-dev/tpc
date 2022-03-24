c2929c94c794ca0e7bd1cde4361e1ce47a3ad226
Boat <gitboat.dev@gmail.com>
2022-03-24 10:08:04 +0700

    24-03-2022 10:07 By Boat

--- resources/views/products/twisted-yarn.blade.php
+++ resources/views/products/twisted-yarn.blade.php
@@ -15,12 +15,9 @@
 					<div class="col-xs-12 col-sm-12 col-md-6 p-0 col2">
 						<div class="content_1">
 							<p class="p1">
-								Twisting is simply the process of combining
-								together multiple fibers, threads or yarns in a
-								wrapping around (twisting) motion. Twisting
-								fibers will create a thread (or singles), twisting
-								threads will create a yarn, and twisting yarns will
-								create a cord or cable - and so on.
+                                Twisted yarn combines multiple fibers, threads, or yarn to wrap around (twisting) motion.
+                                For example, twisting fibers will create a thread (or single), twisting threads will create a
+                                yarn, and twisting yarns will create a cord or cable - and so on.
 							</p>
 						</div>
 					</div>
@@ -30,43 +27,35 @@
 			<div class="section-two bg-black">
 				<div class="row m-0 r1">
 					<div class="col-xs-12 col-sm-12 col-md-4 p-0 col1">
-						<h2 class="section-two-title">What is<br> twisting?</h2>
+						<h2 class="section-two-title">What is<br> twisted yarn?</h2>
 					</div>
 					<div class="col-xs-12 col-sm-12 col-md-4 p-0 col2">
 						<div class="content_1">
 							<p class="p1">
-								The twisting process is when one end of a fiber
-								strand is held and the other end is turned back to
-								form a yarn.
+                                The twisted yarn process is when one end of a fiber strand is held and the other end is
+                                turned back to form a yarn.
 							</p>
 							<p class="p2">
-								For staple fibers, twisting is necessary to turn the
-								fibers into a yarn. Before twisting, the loose fibers
-								are usually coalesced into a fiber strip, and after
-								twisting the outer fibers are squeezed towards
-								the inner layers to create centripetal pressure,
-								which causes the strand to gain friction along the
-								direction of the fiber elongation.
+                                For staple fibers, twisting is necessary to turn the fibers into yarns. Before twisting, the
+                                loose fibers usually coalesce into a fiber strip.<br>
+                                After twisting, the outer fibers are squeezed towards the inner layers to create centripetal
+                                pressure, which causes the strand to gain friction along the direction of the fiber elongation.
+
 							</p>
 						</div>
 					</div>
 					<div class="col-xs-12 col-sm-12 col-md-4 p-0 bg-white col3">
 						<div class="content_1">
 							<p class="p1">
-								For filament yarn and ply yarn, twisting creates a
-								compact structure that is not easily damaged by
-								lateral external forces, and twisting also creates
-								deformed filaments and fancy threads. The
-								amount and direction of twisting affects not only
-								the feel and appearance of the fabric but also its
-								intrinsic quality.
+                                Twisting creates a compact structure that is not easily damaged by lateral external forces,
+                                and twisting also creates deformed filaments and fancy threads for filament yarn and ply
+                                yarn. In addition, the amount and direction of the twist affect the feel and appearance of
+                                fabrics and their intrinsic quality.
 							</p>
 							<p class="p2">
-								After twisting, the outer fiber is twisted back in
-								an inclined spiral, the fiber is twisted and
-								deformed and the yarns are held together,
-								changing the collective structural form and
-								mechanical and physical properties of the fibers.
+                                After twisting, the outer fiber is twisted back in an inclined spiral. The fiber is twisted and
+                                deformed. Yarns are held together, changing the fibers' collective structural form and
+                                mechanical and physical properties.
 							</p>
 						</div>
 					</div>
@@ -89,22 +78,22 @@
 					<div class="col-xs-12 col-sm-12 col-md-8 p-0 col2">
 						<div class="content_1">
 							<h2 class="section-three-title">
-								Types of twisted<br>
-								and how to identify
+                                Types of twisted<br> and how to identify?
 							</h2>
 							<p class="p1">
-								Twisted yarn can be classified into 2 types depending on the direction of the twisted.<br><br>
-								-&nbsp; S twisted yarn is the left twisted<br>
-								-&nbsp; Z twisted yarn is the right twisted<br>
+                                Twisted yarn can be classified into 2 types depending on the twisting direction.<br><br>
+                                - S twisted yarn is the left twisted<br>
+                                - Z twisted yarn is the right twisted<br>
 							</p>
 						</div>
 					</div>
 					<div class="col-xs-12 col-sm-12 col-md-12 p-0 bg-black col3">
 						<div class="content_1">
 							<p class="p1">
-								In the other word, the portion of the fiber on the yarns indicates where the yarn is S or Z twist. Regular weaving industry usually used a Z twisted yarn.
-								The direction of the twisted may observe by holding the yarns in a vertical position. If the twisted direction of the slope of the center part of the letter “S”, the yarn has an S twist.
-								If the yarn spiral conforms to the slope of the letter “Z”, the yarn has a ‘Z” twist.
+                                The portion of fibers on yarns indicates where the yarn is S or Z twist. The regular weaving
+                                industry usually used a Z twisted yarn. The twisting direction may observe by holding yarns
+                                in a vertical position. If the twisting direction of the slope of the center part of the letter “S”,
+                                the yarn has an S twist. If the yarn spiral conforms to the letter “Z” slope, the yarn has a ‘Z” twist.
 							</p>
 						</div>
 					</div>
@@ -116,26 +105,20 @@
 						<div class="content_block">
 							<div class="col-md-12 col1-1">
 								<h2 class="section-four-title">Twist:<br>
-									<small>The number of twisting rounds<br>
-									per unit length of yarn.</small>
+                                    <small>The number of twisting rounds<br> per yarn unit length.</small>
 								</h2>
 							</div>
 							<div class="col-md-12 col1-2">
 								<div class="content_1">
 									<p class="p1">
-										Two sections of the yarn produce an angular
-										displacement of 360°, which becomes a twisting
-										round, commonly referred to as one turn.
+                                        2 sections of the yarn produce an angular displacement of 360°, which becomes a twisting
+                                        round, commonly referred to as one turn.
 									</p>
 									<p class="p2">
-										Yarns are generally twisted in the Tex system,
-										expressed as the number of twisting rounds per
-										10cm of yarn length. For combed woolen yarns
-										and chemical filaments, use the metric count
-										system, expressed as the number of twisting
-										rounds per meter. In addition, there is the English
-										count system, expressed as the number of
-										twisting rounds per inch.
+                                        Yarns are generally twisted in the Tex system, expressed as the number of twisting rounds
+                                        per 10cm of yarn length. For combed woolen yarns and chemical filaments, use the metric
+                                        count system, expressed as the number of twisting rounds per meter. In addition, there is
+                                        the English count system, defined as the number of twisting rounds per inch.
 									</p>
 								</div>
 							</div>
@@ -152,19 +135,13 @@
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 p-0 bg-black col3">
 						<div class="content_1">
 							<p class="p1">
-								The twisted affects the strength, flexibility, elasticity
-								and shrinkage of the yarn. If the twisted
-								increases, the strength of the yarn would be
-								increased, but the twist must not exceed a certain
-								value, otherwise the strength decreases, which is
-								called the critical twist of the yarn. The critical
-								twisted is different for yarns made from different
-								raw materials. In general, as long as the strength
-								requirements are met, the less twist the yarn has,
-								the better, because an increasing in the twisted
-								makes the yarn stiffer to the touch, less elastic
-								and more shrinkage, this is the reason why filament
-								yarns are twisted as little as possible.
+                                The twisted affects the yarn's strength, flexibility, elasticity, and shrinkage. If the twisted
+                                increases, the strength of the yarn would be increased, but the twist must not exceed a
+                                particular value. Otherwise, the strength decreases, which is called the critical twist of the
+                                yarn. The critical twisted is different for yarns made from different raw materials. In general,
+                                as long as the strength requirements are met, the less twist the yarn has, the better,
+                                because an increase in the twisted makes the yarn stiffer to the touch, less elastic and more
+                                shrinkage. This is the reason why filament yarns are twisted as little as possible.
 							</p>
 						</div>
 					</div>
@@ -174,29 +151,26 @@
 				<div class="line-l"></div>
 				<div class="row m-0 r1">
 					<div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
-						<h2 class="section-five-title">How twisted yarn measured?</h2>
+						<h2 class="section-five-title">How is twisted yarn measured?</h2>
 					</div>
 					<div class="col-xs-12 col-sm-12 col-md-5 p-0 col2">
 						<div class="content_1">
 							<p class="p1">
-								Most of fabrics are made by using yarn with an
-								ordinary twisted. This is the amount necessary to
-								hold the fibers close enough together to prevent
-								them from slipping apart. Thus gives strength to
-								the yarn.
+                                Most fabrics are made with ordinary yarn twisted. This is the amount necessary to hold the
+                                fibers close enough together to prevent them from slipping apart. Thus gives strength to the
+                                yarn.
 							</p>
 							<p class="p2">
-								The amount of twisted can be measure by the
-								number of twisted per inch.
+                                The number of twists per inch can measure the number of twists.
 							</p>
 							<p class="p3">
-								Low twisted – 0 to 3 twisted per inch
+                                Low twisted – 0 to 3 twisted per inch
 							</p>
 							<p class="p4">
-								Ordinary twisted – 4 to 7 twisted per inch
+                                Ordinary twisted – 4 to 7 twisted per inch
 							</p>
 							<p class="p5">
-								High twisted – 8 to 12 twisted per inch
+                                High twisted – 8 to 12 twisted per inch
 							</p>
 						</div>
 					</div>
