dd04c613f7b98561c4361625ee6dbb73211020d6
Boat <gitboat.dev@gmail.com>
2022-04-11 16:34:31 +0700

    11-04-2022 16:34 By Boat

--- resources/views/blogs/virgin-polyester-vs-recycled-polyester.blade.php
+++ resources/views/blogs/virgin-polyester-vs-recycled-polyester.blade.php
@@ -24,6 +24,96 @@
                 </div>
 
             </section>
+            <section id="sec2">
+                <div class="row m-0">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                        <div class="content_block pos-r d-flex">
+                            <div class="content_group m-auto">
+                                <p>
+                                    Polyester is a man-made synthetic material that obtains from petroleum widely used in various industries, including the garment industry, interior decoration, automotive, home textile, soft toy,
+                                    accessories, etc. Nowadays, polyester is one of the most used fabric types globally for the textile industry due to its advantageous characteristics such as flexibility, durability, high toughness,
+                                    heat-resistance, stain-resistance, wrinkle resistance, and versatility economical price.
+                                </p>
+                                <p>
+                                    However, global warming and pollution concerns have been the world’s attention nowadays. As virgin polyester production comes from a non-renewable source which somehow creates pollution from
+                                    dyeing and solid waste, we can see that virgin production is unsustainable for the global and environment. As a leader of the polyester supplier in Thailand, Thai Polyester Co., Ltd. (TPC) aims to
+                                    decrease environmental problems from our production and assures us to commit to environmentally friendly operations by focusing on recyclable, reusing, and reducing.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="sec3">
+                <div class="row m-0">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                        <div class="content_block pos-r d-flex">
+                            <div class="content_group m-auto">
+                                <h2 class="theme-hl">What is a substitute for virgin polyester?</h2>
+                                <p>
+                                    Recycled polyester is an alternative and sustainable option to virgin polyester since the production does not depend on petroleum (non-renewable source) as a raw material. Thus, we would reduce
+                                    our reliance on crude oil to continue developing on sustainable and environmentally friendly.<br>
+                                    Polyester recycled yarn starts from collecting plastic bottles and production waste, then cleaning and cutting PET bottles into small pieces called PET flakes and melting flakes into pellets. The next
+                                    process is melted by using high temperature and then spinning those pellets into the polyester recycled polymer.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="sec4">
+                <div class="row m-0">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                        <div class="content_block pos-r d-flex">
+                            <div class="img_block m-auto">
+                                <picture>
+                                    <source srcset="\assets\images\blogs\columns\vp-vs-rp\mobile\Virgin-Polyester-VS-Recycled-Polyester-Recycled-Polyester-Yarn-process-Thaipolyester.webp" alt="Virgin Polyester VS Recycled Polyester Recycled Polyester Yarn process Thaipolyester" media="(max-width: 640px)">
+                                    <img src="\assets\images\blogs\columns\vp-vs-rp\desktop\Virgin-Polyester-VS-Recycled-Polyester-Recycled-Polyester-Yarn-process-Thaipolyester.webp" width="900" height="350" class="img-fluid" alt="Virgin Polyester VS Recycled Polyester Recycled Polyester Yarn process Thaipolyester">
+                                </picture>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="sec5">
+                <div class="row m-0">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                        <div class="content_block pos-r d-flex">
+                            <div class="content_group m-auto">
+                                <h2 class="theme-hl">How can we make the world a better place?</h2>
+                                <p>
+                                    PET bottle recycling process is more sustainable and environmentally since it requires approximately 20% less energy and much less water consumption during its production cycle.
+                                    Using post-consumer and post-industrial, like PET plastic bottles and other production wastes, has major benefits for decreasing the amount of plastic waste from landfills and the ocean, one of the
+                                    main factors in creating and releasing toxic gases into the atmosphere. Referring to our recycled production, a jacket (450 grams) can save 18 bottles, while a blanket (60”x75”) can save up to 26 bottles
+                                    of 1.5 liters water bottle. Additionally, 5,000 grams of recycled PET bottle yarn help save up to 200 bottles of 1.5 liters water bottle.<br>
+                                    Polyester waste recycling reduces overall carbon dioxide emissions by about 30% compared to virgin polyester, resulting in a lower environmental impact and climate change. It also decreases the
+                                    overall carbon footprint as it does not require new petroleum in its production, saving energy consumption and natural resources such as crude oil and natural gas.
+                                    TPC is proud to be a polyester recycling company. In 2019, we established “ECO TPC” a brand-new ECO-friendly product line, with a variety of product ranges to support the increasing demand of
+                                    polyester recycled yarn in local and export customers as below items;
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="sec6">
+                <div class="row m-0">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                        <div class="content_block pos-r d-flex">
+                            <div class="content_group">
+                                <ul>
+                                    <li>- Recycled Polyester Staple Fiber (PSF)</li>
+                                    <li>- Recycled Partially Oriented Yarn (POY)</li>
+                                    <li>- Recycled Drawn Textured Yarn (DTY)</li>
+                                    <li>- Recycled Fully Drawn Yarn (FDY)</li>
+                                    <li>- Recycled Polyester Spun Yarn</li>
+                                    <li>- Recycled Twisted Yarn</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
 @endsection
