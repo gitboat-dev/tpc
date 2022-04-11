ff81c6d30dbca894d0cb94a92728eb5163ef864a
Boat <gitboat.dev@gmail.com>
2022-04-11 15:56:25 +0700

    11-04-2022 15:56 By Boat

--- resources/views/blogs/virgin-polyester-vs-recycled-polyester.blade.php
+++ resources/views/blogs/virgin-polyester-vs-recycled-polyester.blade.php
@@ -5,19 +5,19 @@
 @section('content')
     <div id="tpc">
         <div id="tpc_bg">
-            <section class="sec1">
+            <section id="sec1">
                 <div class="row m-0">
                     <div class="col-xs-12 col-sm-12 col-md-12 p-0">
                         <div class="content_block pos-r d-flex">
-                            <div class="img_block">
+                            <div class="img_block w-100p">
                                 <picture>
                                     <source srcset="\assets\images\blogs\columns\vp-vs-rp\mobile\Head-Virgin-Polyester-VS-Recycled-Polyester-Thaipolyester.webp" alt="Virgin Polyester VS Recycled Polyester" media="(max-width: 640px)">
                                     <img src="\assets\images\blogs\columns\vp-vs-rp\desktop\Head-Virgin-Polyester-VS-Recycled-Polyester-Thaipolyester.webp" width="900" height="350" class="img-fluid" alt="Virgin Polyester VS Recycled Polyester">
                                 </picture>
                             </div>
                             <div class="content_group m-auto pos-ab w-100p h-100p d-flex">
-                                <h1 class="text-center m-auto desktop">Virgin Polyester VS Recycled Polyester</h1>
-                                <h1 class="text-center m-auto mobile">Virgin Polyester<br> VS<br> Recycled Polyester</h1>
+                                <h1 class="h-title text-center m-auto w-100p desktop">Virgin Polyester VS Recycled Polyester</h1>
+                                <h1 class="h-title text-center m-auto w-100p mobile"><span class="m-tl1">Virgin Polyester<br> VS<br> Recycled Polyester</span></h1>
                             </div>
                         </div>
                     </div>
