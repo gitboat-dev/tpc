f3df054ea23c727a4c76ebfe6b7946301e1cf2d9
Boat <gitboat.dev@gmail.com>
2022-04-22 11:10:56 +0700

    22-10-2022 11:10 By Boat

--- resources/views/landing/thank-you/thank1.blade.php
+++ resources/views/landing/thank-you/thank1.blade.php
@@ -6,43 +6,30 @@
 @section('og-image','\assets\images\landing\page1\og-image\og-image.webp')
 @section('content')
 <div id="landing">
-    <div class="thank wow animate__zoomIn">
-        <div class="thank-dialog">
-            <div class="thank-content">
-                <div class="thank-header">
+    <div class="thank-dialog d-flex m-auto">
+        <div class="thank m-auto wow animate__zoomIn">
+            <div class="thank-icon d-flex pos-r">
+                <div class="img_block m-auto">
+                    <picture>
+                        <source srcset="\assets\images\landing\page1\thank-you\mobile\mark-succeed.webp" alt="Thank you" media="(max-width: 640px)" loading="lazy">
+                        <img src="\assets\images\landing\page1\thank-you\desktop\mark-succeed.webp" width="900" height="350" class="img-fluid ab-img" alt="Thank you" loading="lazy">
+                    </picture>
                 </div>
-                <div class="thank-body d-flex pos-r">
-                    <div class="thank-group mb-0">
-                        <div class="thank-icon pos-ab w-100p d-flex">
-                            <div class="img_block">
-                                <picture>
-                                    <source srcset="\assets\images\landing\page1\thank-you\mobile\mark-succeed.webp" alt="Thank you" media="(max-width: 640px)" loading="lazy">
-                                    <img src="\assets\images\landing\page1\thank-you\desktop\mark-succeed.webp" width="900" height="350" class="img-fluid ab-img" alt="Thank you" loading="lazy">
-                                </picture>
-                            </div>
-                        </div>
-                        <h3 class="thank-title">Thank you!</h3>
-                        <p class="thank-desc">We'll get back to you within 24 hours by <b>support@thaipolyester.com</b>
-                            Our e-mail might have gone through your Junk-mail. Kindly check in it ;)</p>
-                    </div>
-                </div>
-                <div class="thank-footer d-flex">
-                    <div class="thank-group mt-50">
-                        {{-- <div class="thank-close">
-                            <div class="img_block d-flex">
-                                <picture>
-                                    <source srcset="\assets\images\landing\page1\thank-you\mobile\mark-close.webp" alt="Thank you" media="(max-width: 640px)" loading="lazy">
-                                    <img src="\assets\images\landing\page1\thank-you\desktop\mark-close.webp" width="900" height="350" class="img-fluid ab-img" alt="Thank you" loading="lazy">
-                                </picture>
-                            </div>
-                        </div> --}}
-                        <div class="thank-web text-center">
-                            <a href="/" title="Thai polyester">
-                                go to website
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                <div class="thank-icon-bg pos-ab bottom-0 w-100p"></div>
+            </div>
+            <div class="thank-body">
+                <h3 class="thank-title text-center">
+                    Thank you!
+                </h3>
+                <p class="thank-desc text-center">
+                    We'll get back to you within 24 hours by <b>support@thaipolyester.com</b>
+                    Our e-mail might have gone through your Junk-mail. Kindly check in it ;)
+                </p>
+            </div>
+            <div class="thank-web text-center">
+                <a href="/" title="Thai polyester">
+                    go to website
+                </a>
             </div>
         </div>
     </div>
