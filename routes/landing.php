51c864ef4fa3ee02ab4f772e51e8d9f43f8c3690
Boat <gitboat.dev@gmail.com>
2022-04-22 15:20:25 +0700

    22-04-2022 15:20 By Boat

--- routes/landing.php
+++ routes/landing.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'landing'], function () {
     Route::get('/High-Quality-Polyester-Yarn-and-Fiber', 'landingPageController@page1');
     Route::get('/landing/thank-you', 'landingPageController@thank_you1');
-    Route::get('/landing/thank-you2', 'landingPageController@thank_you1');
+    // Route::get('/landing/thank-you2', 'landingPageController@thank_you1');
     Route::get('/landing/thank-you3', 'landingPageController@thank_you1');
     Route::get('/en/polyester-yarn-spun-filament-fiber-thailand-manufacturers-suppliers-free-sample', 'landingPageController@page2');
     Route::get('/th/polyester-yarn-spun-filament-fiber-thailand-manufacturers-suppliers-free-sample', 'landingPageController@page3');
