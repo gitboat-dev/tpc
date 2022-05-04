518eb72e62216ccf8c9eb5c71165710cf1bdd8e2
Boat <gitboat.dev@gmail.com>
2022-05-04 09:36:07 +0700

    04-05-2022 09:36 By Boat

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -15,14 +15,6 @@
             <div class="row m-0">
                 <div class="col-xs-12 col-sm-12 col-md-12 p-0">
                     <div class="img_block">
-                        {{-- @if($blogs && isset($blogs))
-                            @if(isset($blogs[0]['cover']['url']))
-                                <picture>
-                                    <source srcset="{{$blogs[0]['cover']['url']}}" alt="{{$head_line}}" media="(max-width: 640px)">
-                                    <img src="{{$blogs[0]['cover']['url']}}" width="900" height="350" class="img-fluid" alt="{{$head_line}}">
-                                </picture>
-                            @endif
-                        @endif --}}
                         @foreach($blogs as $b)
                             @if(isset($b['banner']) && $b['banner'])
                                 <picture>
