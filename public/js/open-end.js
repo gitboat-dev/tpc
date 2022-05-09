49e8f39ecd4d1fc8714c7e9951d7008d61405ee9
Boat <gitboat.dev@gmail.com>
2022-05-09 12:01:39 +0700

    09-05-2022 12:01 By Boat

--- public/js/open-end.js
+++ public/js/open-end.js
@@ -1,12 +1,24 @@
 jQuery(document).ready(function($){
     sec1();
+    sec2();
     $(window).resize(function(event) {
         sec1();
+        sec2();
     });
 });
 function sec1(){
 	var row1 = jQuery('#sec1 .r1 .col1 .content_block'),
 		row2 = jQuery('#sec1 .r1 .col2 .content_block'),
+		minheight = jQuery(row1).outerHeight();
+	if(jQuery(window).width() <= 991){
+		jQuery(row2).removeAttr('style');
+	}else{
+		jQuery(row2).css({'min-height':minheight+'px'});
+	}
+}
+function sec2(){
+	var row1 = jQuery('#sec2 .r1 .col1 .content_block'),
+		row2 = jQuery('#sec2 .r1 .col2 .content_block'),
 		minheight = jQuery(row1).outerHeight();
 	if(jQuery(window).width() <= 991){
 		jQuery(row2).removeAttr('style');
--- resources/views/products/open-end.blade.php
+++ resources/views/products/open-end.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 <div id="tpc">
     <div id="tpc_bg">
-        <section id="sec1" class="bg-black">
+        <section id="sec1" class="bg-black pos-r">
             <div class="row m-0 r1">
-                <div class="col-xs-12 col-sm-12 col-md-6 col1 pos-r">
+                <div class="col-xs-12 col-sm-12 col-md-6 col1">
                     <div class="content_block pos-r d-flex">
                         <div class="img_block w-100p">
                             <picture>
@@ -25,6 +25,42 @@
                                 or<br>
                                 OE SPUN
                             </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="sec2">
+            <div class="row m-0 r1">
+                <div class="col-xs-12 col-sm-12 col-md-4 col1">
+                    <div class="content_block pos-r">
+                        <div class="content_group">
+                            <h2 class="theme-hl fw-600">
+                                The heart of the<br>
+                                open-end process<br>
+                                is a rotor,
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-8 bg-white col2">
+                    <div class="content_block pos-r d-flex">
+                        <div class="content_group m-auto">
+                            <p>
+                                wherein fibers can be collected and then drawn off as a yarn. For short staple spinning, most rotors are 31 to 56
+                                millimeters in diameter and may contain a shallow "U" or "V" shaped fiber alignment groove around their periphery. In
+                                open end spinning, the rotor rotation provides the twisting force.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 bg-white pos-r col3">
+                    <div class="content_block pos-r d-flex">
+                        <div class="img_block w-100p">
+                            <picture>
+                                <source srcset="/assets/images/open-end/mobile/Open-End-or-OE-spun-Process-Thaipolyester.webp" alt="Open End or OE spun Process Thaipolyester" media="(max-width: 640px)" loading="lazy">
+                                <img src="/assets/images/open-end/desktop/Open-End-or-OE-spun-Process-Thaipolyester.webp" width="900" height="350" class="img-fluid" alt="Open End or OE spun Process Thaipolyester" loading="lazy">
+                            </picture>
                         </div>
                     </div>
                 </div>
