c38b4dfcfca9b988459b23962c671136fe8d4a05
Boat <gitboat.dev@gmail.com>
2022-05-12 09:37:28 +0700

    12-05-2022 09:37 By Boat

--- public/js/open-end.js
+++ public/js/open-end.js
@@ -1,9 +1,15 @@
 jQuery(document).ready(function($){
     sec1();
     sec2();
+    sec3();
+    sec4();
+    sec5();
     $(window).resize(function(event) {
         sec1();
         sec2();
+        sec3();
+        sec4();
+        sec5();
     });
 });
 function sec1(){
@@ -25,4 +31,35 @@ function sec2(){
 	}else{
 		jQuery(row2).css({'min-height':minheight+'px'});
 	}
+}
+function sec3(){
+	var row1 = jQuery('#sec3 .r1 .col1 .content_block'),
+		row2 = jQuery('#sec3 .r1 .col2 .content_block'),
+		minheight = jQuery(row1).outerHeight();
+	if(jQuery(window).width() <= 991){
+		jQuery(row2).removeAttr('style');
+	}else{
+		jQuery(row2).css({'min-height':minheight+'px'});
+	}
+}
+function sec4(){
+	var row1 = jQuery('#sec4 .r1 .col1 .content_block'),
+		row2 = jQuery('#sec4 .r1 .col2 .content_block'),
+		minheight = jQuery(row2).outerHeight();
+        console.log(minheight);
+	if(jQuery(window).width() <= 991){
+		jQuery(row1).removeAttr('style');
+	}else{
+		jQuery(row1).css({'min-height':minheight+'px'});
+	}
+}
+function sec5(){
+    var row1 = jQuery('#sec5 .r1 .col1 .content_block'),
+        text_block = jQuery('#sec5 .r1 .col1 .content_block .text-block'),
+        minheight = jQuery(row1).outerHeight();
+    if(jQuery(window).width() <= 1132){
+        jQuery(text_block).removeAttr('style');
+    }else{
+        jQuery(text_block).css({'min-height':minheight+'px'});
+    }
 }
--- resources/views/products/open-end.blade.php
+++ resources/views/products/open-end.blade.php
@@ -66,24 +66,26 @@
                 </div>
             </div>
         </section>
-        <section id="sec3">
+        <section id="sec3" class="bg-black">
             <div class="row m-0 r1">
-                <div class="col-xs-12 col-sm-12 col-md-8 col1">
-                    <div class="video_1">
-                        <video muted loop id="video1" poster="{{ $chkmobile ? "/assets/images/open-end/mobile/Open-End-or-OE-spun-Video-Process-Thaipolyester.webp" : "/assets/images/open-end/desktop/Open-End-or-OE-spun-Video-Process-Thaipolyester.webp" }}">
-                            <source src="/assets/images/open-end/desktop/video/minify/C0334.mp4" type="video/mp4">
-                        </video>
-                        <figure>
-                            <button class="button" id="btn-video1" name="play">
-                                <picture>
-                                    <source srcset="/assets/images/open-end/mobile/Play-Icon.webp" alt="Play video" loading="lazy" media="(max-width: 640px)">
-                                    <img src="/assets/images/open-end/desktop/Play-Icon.webp" width="900" height="350" class="img-fluid ab-img" alt="Play video" loading="lazy">
-                                </picture>
-                            </button>
-                        </figure>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 bg-white col1">
+                    <div class="content_block pos-r">
+                        <div class="video_1">
+                            <video muted loop id="video1" poster="{{ $chkmobile ? "/assets/images/open-end/mobile/Open-End-or-OE-spun-Video-Process-Thaipolyester.webp" : "/assets/images/open-end/desktop/Open-End-or-OE-spun-Video-Process-Thaipolyester.webp" }}">
+                                <source src="/assets/images/open-end/desktop/video/minify/C0334.mp4" type="video/mp4">
+                            </video>
+                            <figure>
+                                <button class="button" id="btn-video1" name="play">
+                                    <picture>
+                                        <source srcset="/assets/images/open-end/mobile/Play-Icon.webp" alt="Play video" loading="lazy" media="(max-width: 640px)">
+                                        <img src="/assets/images/open-end/desktop/Play-Icon.webp" width="900" height="350" class="img-fluid ab-img" alt="Play video" loading="lazy">
+                                    </picture>
+                                </button>
+                            </figure>
+                        </div>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-4 bg-black col2">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 bg-black col2">
                     <div class="content_block pos-r d-flex">
                         <div class="content_group m-auto">
                             <p class="theme-hl" style="text-indent: 30px">
@@ -112,6 +114,92 @@
                                 stripping of the rotor.
                             </p>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="sec4">
+            <div class="row r1 m-0">
+                <div class="col-xs-12 col-sm-12 col-md-6 pos-r p-0 col1">
+                    <div class="content_block pos-r">
+                        <h2 class="h1 theme-hl">Differences:</h2>
+                        <div class="text-block d-flex">
+                            <p class="ml-0">
+                                The basic difference between ring-spun yarns and
+                                open-end spun yarns is in the way they are
+                                formed. The former produces yarn by inserting
+                                twist into a continuous ribbon-like strand of
+                                cohesive fibers delivered by the front rolls, while
+                                the latter forms yarn from individual fibers
+                                directly by collecting them from the inside surface
+                                of a rotor by twist forces. Thus, for comparison, it
+                                could be said that a ring yarn is formed from the
+                                outside in, while open-end yarn is formed from
+                                the inside out.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 pos-r p-0 col2">
+                    <div class="content_block d-flex pos-r">
+                        <div class="img_block w-100p">
+                            <picture>
+                                <source srcset="/assets/images/open-end/mobile/Open-End-or-OE-spun-Peocess-Thaipolyester.webp" alt="OE Spun Open End Polyester yarn machine Thaipolyester" media="(max-width: 640px)">
+                                <img src="/assets/images/open-end/desktop/Open-End-or-OE-spun-Peocess-Thaipolyester.webp" width="900" height="350" class="img-fluid" alt="OE Spun Open End Polyester yarn machine Thaipolyester">
+                            </picture>
+                        </div>
+                        <div class="text-block pos-ab bg-black">
+                            <h2 class="white-hl fw-600 m-0">
+                                open end<br>
+                                spun yarns
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="sec5" class="pos-r bg-black">
+            <div class="row r1 m-0">
+                <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                    <div class="content_block pos-r">
+                        <div class="img_block w-100p">
+                            <picture>
+                                <source srcset="/assets/images/open-end/mobile/OE-Spun-Open-End-Polyester-yarn-machine-Process-Thaipolyester.webp" alt="OE Spun Open End Polyester yarn machine Process Thaipolyester" media="(max-width: 640px)">
+                                <img src="/assets/images/open-end/desktop/OE-Spun-Open-End-Polyester-yarn-machine-Process-Thaipolyester.webp" width="900" height="350" class="img-fluid" alt="OE Spun Open End Polyester yarn machine Process Thaipolyester">
+                            </picture>
+                        </div>
+                        <div class="text-block d-flex">
+                            <p class="m-auto bg-black">
+                                The elements basic to production of open-end
+                                yarns are somewhat different from ring spinning.
+                                They are:<br>
+                                a) Fiber supply<br>
+                                b) Drafting system<br>
+                                c) Fiber collection and alignment<br>
+                                d) Twist insertion -- yarn formation<br>
+                                e) Package winding<br>
+                                Spun yarn luster<br>
+                                Luster refers to the degree of light that is
+                                reflected from the surface of a fiber or the degree
+                                of gloss or sheen that the fiber possesses.<br>
+                                - Full dull<br>
+                                - Semi-dull<br>
+                                - Bright
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <section id="sec6" class="bg-black">
+            <div class="row r1 m-0">
+                <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                    <div class="img_block bg-green w-100p">
+                        <picture>
+                            <source srcset="/assets/images/open-end/mobile/Open-End-or-OE-spun-Product-Polyester.webp" alt="Open End or OE spun Product Polyester" media="(max-width: 640px)">
+                            <img src="/assets/images/open-end/desktop/Open-End-or-OE-spun-Product-Polyester.webp" width="900" height="350" class="img-fluid" alt="Open End or OE spun Product Polyester">
+                        </picture>
                     </div>
                 </div>
             </div>
