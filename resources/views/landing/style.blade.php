c6355779709ea9ce1335308586aeb8a26ce4d864
Boat <gitboat.dev@gmail.com>
2022-05-25 13:49:56 +0700

    25-05-2022 13:49 By Boat

--- resources/views/landing/style.blade.php
+++ resources/views/landing/style.blade.php
@@ -1,6 +1,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap" rel="stylesheet">
+{{-- <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap" rel="stylesheet"> --}}
+<link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;600;800&family=Sen:wght@400;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/assets/lib/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <link rel="stylesheet"  href="/css/landing/template.min.css?v={{ time() }}" type="text/css" media="all">
