2b6fc71c58ad496230edfa62e1abb5aa3c1f0540
Boat <gitboat.dev@gmail.com>
2022-05-28 15:47:46 +0700

    28-05-2022 15:47 By Boat

--- app/Http/Middleware/PDPA.php
+++ app/Http/Middleware/PDPA.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Contracts\Routing\Middleware;
 
-class PDPA {
+class pdpa {
 	public function handle($request, Closure $next) {
 		return $next($request);
 	}
