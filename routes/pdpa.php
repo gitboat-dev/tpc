10170c8e7579397decec6e2c6d2f091ac6e3b7e5
Boat <gitboat.dev@gmail.com>
2022-06-02 08:22:34 +0700

    02-06-2022 08:21 By Boat

--- routes/pdpa.php
+++ routes/pdpa.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'pdpa','prefix' => 'pdpa'], function () {
     Route::get('/', 'CookieController@check');
 	Route::get('{event}', 'CookieController@event');
-    Route::get('/content/privacy-policy', function(){
-        return view('layouts.privacy_policy.content');
-    });
 });
+// Route::get('content/privacy-policy', function(){
+//     return view('layouts.privacy_policy.content');
+// });
