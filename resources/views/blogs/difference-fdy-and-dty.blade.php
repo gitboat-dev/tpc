6282df39bee40cc54950e76f5a51525639f1ea66
Boat <gitboat.dev@gmail.com>
2022-06-23 13:42:51 +0700

    23-06-2022 13:42 By Boat

---
+++ resources/views/blogs/difference-fdy-and-dty.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.template')
+@section('style')
+    <link rel="preload stylesheet" href="/css/blogs/difference-fdy-and-dty.css?v={{time()}}" as="style" crossorigin>
+@endsection
+@section('og-image','')
+@section('content')
+<div id="tpc">
+    <div id="tpc_bg">
+        <section id="sec1">
+            <div class="row m-0">
+                <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                    <div class="content pos-r d-flex">
+                        <div class="img_block w-100p">
+                            <picture>
+                                <source srcset="\assets\images\blogs\columns\difference-FDY-and-DTY\mobile\How-difference-between-FDY-and-DTY-Yarn-Head-Thaipolyester.webp?v={{time()}}" media="(max-width: 640px)">
+                                <img src="\assets\images\blogs\columns\difference-FDY-and-DTY\desktop\How-difference-between-FDY-and-DTY-Yarn-Head-Thaipolyester.webp?v={{time()}}" width="900" height="350" class="img-fluid" alt="How difference between FDY and DTY Yarn Head Thaipolyester">
+                            </picture>
+                        </div>
+                        <div class="pos-ab w-100p h-100p d-flex">
+                            <h1 class="head-title fw-600 text-center m-auto">
+                                <span class="m-tl1">
+                                    What is the<br>
+                                    difference between<br>
+                                    FDY and DTY Yarn
+                                </span>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="sec2">
+            <div class="row m-0 d-flex m-flex-c">
+                <div class="col-xs-12 col-sm-12 col-md-6 p-0 m-auto col1">
+                    <div class="content d-flex">
+                        <div class="content_group m-auto">
+                            <h2 class="theme-hl"><span class="tl1 m-tl1">FDY (Fully Drawn Yarn)</span></h2>
+                            <p>
+                                FDY is also known as Polyester Filament Yarn (PFY) or Spin Draw Yarn (SDY).
+                                FDY manufacturing process is similar to the POY manufacturing process. But
+                                FDY is produced at higher speeds than the POY manufacturing process, and
+                                intermediate drawing integrated into stretching, orientation and crystallization
+                                is more stable. This yarn is used for direct twisting, multiplies yarns, beaming,
+                                and direct knitting. And FDY threads are mostly used to make non-textured
+                                fabrics such as hospital clothes, pads, surgical sets, wound healing, wet tissues,
+                                sanitary napkins, baby diapers, coatings, insulating, flooring, carpeting, etc.
+                            </p>
+                            <p>
+                                FDY is mainly used as weft or weaves in producing fabrics. But it also can
+                                combine knitted or woven in various filaments to get various styles.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 p-0 col2">
+                    <div class="img_block w-100p">
+                        <picture>
+                            <source srcset="\assets\images\blogs\columns\difference-FDY-and-DTY\desktop\FDY-(Fully-Drawn-Yarn)-Thaipolyester.webp?v={{time()}}" media="(max-width: 640px)">
+                            <img src="\assets\images\blogs\columns\difference-FDY-and-DTY\desktop\FDY-(Fully-Drawn-Yarn)-Thaipolyester.webp?v={{time()}}" width="900" height="350" class="img-fluid" alt="FDY (Fully Drawn Yarn) Thaipolyester" loading="lazy">
+                        </picture>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="sec3" class="bg-black">
+            <div class="container">
+                <div class="row m-0">
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0 col1">
+                        <div class="content pos-r d-flex">
+                            <div class="content_group m-auto">
+                                <p>
+                                    FDY application has a wide range. It is a type of highly drawn filament yarn that can be used to produce high-strength fabrics and textiles and in a home furnishing fabrics, fashion fabrics,
+                                    Denim, Terry towel, etc. Sometimes, it is also used to produce high-end undergarments, sportswear, home furnishings and non-textured fabrics.
+                                </p>
+                                <p>
+                                    FDY thread is used in different types of shrinkage and areas of the textile industry. For example, FDY threads with a high shrinkage are used in fabricating cloth and curtain fabrics and create a
+                                    blooming structure on the fabric surface. On the other hand, FDY threads with a low shrinkage are widely used in the blanket industry.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="sec4">
+        </section>
+        <section id="sec5" class="bg-black">
+            <div class="container">
+                <div class="row m-0 d-flex m-flex-c">
+                    <div class="col-xs-12 col-sm-12 col-md-6 m-auto col1">
+                        <div class="content pos-r d-flex">
+                            <div class="content_group m-auto">
+                                <h2 class="theme-hl"><span class="tl1 m-tl1">The difference between FDY and DTY Yarn</span></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-6 m-auto col2">
+                        <div class="content pos-r d-flex">
+                            <div class="content_group m-auto">
+                                <p>
+                                    - Processes of manufacturing: FDY is drawn and DTY is making texture.<br>
+                                    - FDY has high strength than DTY because it is fully drawn yarn.<br>
+                                    - FDY cannot stretch much as DTY. Some products don’t need a stretch. FDY
+                                    is more suitable than DTY.<br>
+                                    - DTY is softer than FDY because it has a soft crimp texture. The texture will
+                                    give a soft hand feel. It is usually produced in upholstery or skin-clinging
+                                    garment.<br>
+                                    - DTY has more stretch because texture also gives flexibility as it brings the
+                                    produce of some items which need stretching.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+@endsection
+@section('custom-script')
+<script src="/js/blogs/difference-fdy-and-dty.min.js?v={{time()}}"></script>
+@endsection
