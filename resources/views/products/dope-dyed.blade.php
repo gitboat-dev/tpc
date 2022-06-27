311e4ed49b2bfed0f3112aad8a08620987d7951d
Boat <gitboat.dev@gmail.com>
2022-06-27 09:48:42 +0700

    27-06-2022 09:48 By Boat

--- resources/views/products/dope-dyed.blade.php
+++ resources/views/products/dope-dyed.blade.php
@@ -147,6 +147,99 @@
                 </div>
             </div>
         </section>
+        <section id="sec4" class="pos-r">
+            <div class="row m-0 d-flex m-flex-c">
+                <div class="col-xs-12 col-sm-12 col-md-6 p-0 col1">
+                    <div class="img_block w-100p">
+                        <picture>
+                            <source srcset="\assets\images\dope-dyed\mobile\Dope-Dyed-Polyester-yarn-machine-Process-Thaipolyester-1.webp?v={{time()}}" media="(max-width: 640px)">
+                            <img src="\assets\images\dope-dyed\desktop\Dope-Dyed-Polyester-yarn-machine-Process-Thaipolyester-1.webp?v={{time()}}" width="900" height="350" class="img-fluid" alt="Dope-Dyed Polyester yarn machine-Thaipolyester">
+                        </picture>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 p-0 m-auto pos-r col2">
+                    <div class="d-flex">
+                        <h2 class="sec4-title fw-600">
+                            <span class="m-tl">
+                                <small style="color: inherit;">There are 3 advantages of using</small><br>
+                                <span class="theme-hl">Dope Dyed Polyester Yarn</span><br>
+                                <small style="color: inherit;">for</small> <span class="theme-hl">finished goods</span>
+                            </span>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="sec5" class="bg-black pos-r">
+            <div class="container">
+                <div class="row r1 m-0">
+                    <div class="col-xs-12 col-sm-12 col-md-4">
+                        <div class="content pos-r">
+                            <div class="content_group">
+                                <p>
+                                    <span class="theme-hl fw-600" style="font-size: 24px">Quality:</span> Dope Dyed Polyester Yarn
+                                    is better for wash and perspiration fastness
+                                    (rating of 4-5 for both wash & perspiration
+                                    fastness against the rating of 3 for
+                                    piece-dyed fabrics) even though there is no
+                                    significant difference in rubbing and
+                                    sublimation fastness.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4">
+                        <div class="content pos-r">
+                            <div class="content_group">
+                                <p>
+                                    <span class="theme-hl fw-600" style="font-size: 24px">Cost:</span> Dope Dyed Polyester Yarn is about
+                                    cheaper compared to conventional yarn dyed.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4">
+                        <div class="content pos-r">
+                            <div class="content_group">
+                                <p>
+                                    <span class="theme-hl fw-600" style="font-size: 24px">Less Process:</span> As the fiber is
+                                    already dyed, the process of dyeing is
+                                    completely eliminated leading to the saving
+                                    of a large number of resources. This results in
+                                    less lead time in the production process and
+                                    enables you to deliver your goods faster to
+                                    your customers.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="sec6" class="pos-r">
+            <div class="container">
+                <div class="row r1 m-0">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col1">
+                        <div class="content pos-r d-flex">
+                            <div class="content_group m-auto">
+                                <p>
+                                    With these 3 benefits altogether, you can see the increase in the efficiency of your production process and your finished goods. Eventually, this will increase the satisfactory among
+                                    you and your valuable customers.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 p-0 col2">
+                        <div class="img_block w-100p">
+                            <picture>
+                                <source srcset="\assets\images\dope-dyed\mobile\Dope-Dyed-Polyester-yarn-Product-Thaipolyester.webp?v={{time()}}" media="(max-width: 640px)">
+                                <img src="\assets\images\dope-dyed\desktop\Dope-Dyed-Polyester-yarn-Product-Thaipolyester.webp?v={{time()}}" width="900" height="350" class="img-fluid" alt="Dope-Dyed Polyester yarn machine-Thaipolyester">
+                            </picture>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </div>
 @endsection
