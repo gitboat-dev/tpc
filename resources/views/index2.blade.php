eb4384075a65f78e2ae2c61c506f8a36bd510908
Boat <gitboat.dev@gmail.com>
2022-07-13 14:13:46 +0700

    13-07-2022 14:13 By Boat

---
+++ resources/views/index2.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.template')
+@section('style')
+@endsection
+@section('og-image','')
+@section('content')
+    <div id="tpc">
+        <div id="tpc_bg">
+        </div>
+    </div>
+@endsection
+@section('custom-script')
+@endsection
+
