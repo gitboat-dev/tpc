3185e1cf36bd73f88559f8eef94191769bd46fa8
Boat <gitboat.dev@gmail.com>
2022-07-18 13:41:53 +0700

    18-07-2022 13:41 By Boat

--- resources/views/blogs/new.blade.php
+++ resources/views/blogs/new.blade.php
@@ -11,7 +11,7 @@
 @section('content')
     <div id="tpc">
         <div id="tpc_bg">
-            <section id="sec1" class="bg-black">
+            <section id="sec1" class="pos-r bg-white">
                 @if(isset($blog) && $blog)
                     @if(count($blog->banner) > 1)
                         <div class="slide">
@@ -30,17 +30,33 @@
                         </div>
                     @else
                         @foreach($blog->banner as $banner)
-                            <div class="item p-0 pos-r">
-                                <div class="img_block w-100p">
-                                    <picture>
-                                        <source srcset="{{$banner['url']}}" media="(max-width: 640px)">
-                                        <img src="{{$banner['url']}}" width="900" height="350" class="img-fluid" alt="polyester Yarn process Thaipolyester">
-                                    </picture>
+                            <div class="row m-0">
+                                <div class="col-xs-12 col-sm-12 col-md-12 p-0">
+                                    <div class="img_block w-100p">
+                                        <picture>
+                                            <source srcset="{{$banner['url']}}" media="(max-width: 640px)">
+                                            <img src="{{$banner['url']}}" width="900" height="350" class="img-fluid" alt="polyester Yarn process Thaipolyester">
+                                        </picture>
+                                    </div>
                                 </div>
                             </div>
                         @endforeach
                     @endif
                 @endif
+            </section>
+            <section id="sec2" class="pos-r">
+                <div class="container">
+                    <div class="row m-0">
+                        <div class="col-xs-12 col-sm-12 col-md-8 p-0">
+                            <div class="title-block">
+                                <h1 class="mt-0 fw-600 default">{{$blog->article_title}}</h1>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 p-0">
+                            222
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     </div>
